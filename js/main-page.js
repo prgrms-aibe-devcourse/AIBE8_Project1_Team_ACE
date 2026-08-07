7995d5ca3c7745ab4ef963b2236704380f3313d4
qkdtkd77 <qkdtkd77@gmail.com>
2026-08-07 17:45:52 +0900

    Feat: 페이지네이션 렌더링 로직 구현

--- js/main-page.js
+++ js/main-page.js
@@ -6,13 +6,17 @@
  * ======================================
  */
 
+let allFestivals = [];
+let currentPage = 1;
+const PAGE_SIZE = 4;
+
 const getFestivalList = async () => {
   const container = $("#festival-grid");
   showLoading(container);
 
-  const festivals = await getFestivals();
+  allFestivals = await getFestivals();
 
-  renderFestivalList(festivals);
+  renderFestivalList();
 };
 
 const createFestivalCardHTML = (festival) => {
@@ -30,9 +34,42 @@ const createFestivalCardHTML = (festival) => {
   `;
 };
 
-const renderFestivalList = (festivals) => {
+const getCurrentPageFestivals = () => {
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+  return allFestivals.slice(startIndex, endIndex);
+};
+
+const handleNextPageClick = () => {
+  currentPage += 1;
+  renderFestivalList();
+};
+
+const handlePrevPageClick = () => {
+  currentPage -= 1;
+  renderFestivalList();
+};
+
+const renderPagination = () => {
+  const totalPages = Math.ceil(allFestivals.length / PAGE_SIZE);
+  let pageNumbers = [];
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+  const container = $("#pagination");
+
+  container.innerHTML = pageNumbers.map(createPageButtonHTML).join("");
+};
+
+const createPageButtonHTML = (pageNumber) => {
+  return `<button class="${pageNumber === currentPage ? "page-num-btn active" : "page-num-btn"}" data-page="${pageNumber}">${pageNumber}</button>`;
+};
+
+const renderFestivalList = () => {
   const container = $("#festival-grid");
   if (!container) return;
+
+  const festivals = getCurrentPageFestivals();
 
   if (festivals.length === 0) {
     showEmpty(container, "축제가 없습니다.");
@@ -57,4 +94,5 @@ const handleCardClick = (event) => {
 
 document.addEventListener("DOMContentLoaded", () => {
   getFestivalList();
+  renderPagination();
 });
